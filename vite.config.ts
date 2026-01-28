a6dc8cb2fd4dc8439c1ebf1a50c0c10fe7198f5f
Marco Blumendorf <marco@blumendorf.info>
2026-01-28 20:17:34 +0100

    fix: build

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,14 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  return {
-    plugins: [react()],
-    base: '/',
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src'),
-      },
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
-  }
+  },
 })
