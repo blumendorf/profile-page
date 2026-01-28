2627c6564b30456a104587163eed1e5526ee9b2c
Marco Blumendorf <marco@blumendorf.info>
2026-01-28 20:14:20 +0100

    fix: routing on gh pages and blumendorf.info

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   return {
     plugins: [react()],
-    base: env.DEPLOY_TARGET === 'gh-pages' ? '/profile-page/' : '/',
+    base: '/',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src'),
